4351a75c8b1f71b6229aad675a04922c21a5b052
Jonas Hess <jonas@sfxonline.de>
2023-12-14 17:49:04 +0100

    switch to multi-shop-feature, add check for dumpfile

--- dump-jtl5-database.sh
+++ dump-jtl5-database.sh
@@ -9,28 +9,45 @@
 mydir=$(dirname $0)
 cd "$mydir"
 
-configfile="config.json"
-name="$(jq -r '.name' $configfile)"
-webroot="$(jq -r '.webroot' $configfile)"
-host="$(jq -r '.host' $configfile)"
+configfile="shops.json"
 mysqldump="$(which mysqldump)"
 
-echo "Dumping: $name"
-echo "---------"
+if [ $# -ne 1 ]; then
+    echo "Please enter the shops shortname as parameter."
+    exit 1
+fi
 
-echo "- Get Config for MySQL-Data transfer"
+shop_shortname=$1
+
+if [ ! -f "$configfile" ]; then
+    echo "Could not find the file $configfile."
+    exit 1
+fi
+
+shop_info=$(jq --arg shortname "$shop_shortname" '.[] | select(.shortname == $shortname)' "$configfile")
+
+if [ -z "$shop_info" ]; then
+    echo "Shop $shop_shortname not found in $configfile."
+    exit 1
+fi
+
+name=$(jq -r '.name' <<< "$shop_info")
+webroot=$(jq -r '.webroot' <<< "$shop_info")
+host=$(jq -r '.host' <<< "$shop_info")
+
 remote_config=$(ssh "$host" "cat $webroot/includes/config.JTL-Shop.ini.php")
 remote_config=$(echo "$remote_config" | grep '^[ ]*[^/][^/]*')
+
 remote_mysql_host=$(echo "$remote_config" | grep -o 'DB_HOST",[ ]*"[^"]*' | cut -d '"' -f 3)
 [ -z "$remote_mysql_host" ] && remote_mysql_host=$(echo "$remote_config" | grep -o 'DB_HOST'"'"',[ ]*'"'"'[^'"'"']*' | cut -d ''"'"'' -f 3)
 
-remote_mysql_user=$(echo "$remote_mysql_user" | grep -o 'DB_USER",[ ]*"[^"]*' | cut -d '"' -f 3)
+remote_mysql_user=$(echo "$remote_config" | grep -o 'DB_USER",[ ]*"[^"]*' | cut -d '"' -f 3)
 [ -z "$remote_mysql_user" ] && remote_mysql_user=$(echo "$remote_config" | grep -o 'DB_USER'"'"',[ ]*'"'"'[^'"'"']*' | cut -d ''"'"'' -f 3)
 
-remote_mysql_password=$(echo "$remote_mysql_password" | grep -o 'DB_PASS",[ ]*"[^"]*' | cut -d '"' -f 3)
+remote_mysql_password=$(echo "$remote_config" | grep -o 'DB_PASS",[ ]*"[^"]*' | cut -d '"' -f 3)
 [ -z "$remote_mysql_password" ] && remote_mysql_password=$(echo "$remote_config" | grep -o 'DB_PASS'"'"',[ ]*'"'"'[^'"'"']*' | cut -d ''"'"'' -f 3)
 
-remote_mysql_database=$(echo "$remote_mysql_database" | grep -o 'DB_NAME",[ ]*"[^"]*' | cut -d '"' -f 3)
+remote_mysql_database=$(echo "$remote_config" | grep -o 'DB_NAME",[ ]*"[^"]*' | cut -d '"' -f 3)
 [ -z "$remote_mysql_database" ] && remote_mysql_database=$(echo "$remote_config" | grep -o 'DB_NAME'"'"',[ ]*'"'"'[^'"'"']*' | cut -d ''"'"'' -f 3)
 remote_mysql_port=3306
 
@@ -38,6 +55,10 @@ if [[ $remote_mysql_host == *":"* ]]; then
   remote_mysql_port=$(echo "$remote_mysql_host" | cut -d ':' -f 2)
   remote_mysql_host=$(echo "$remote_mysql_host" | cut -d ':' -f 1)
 fi
+
+echo "Dumping: $name"
+echo "---------"
+
 echo "Host:     $remote_mysql_host"
 echo "Port:     $remote_mysql_port"
 echo "Database: $remote_mysql_database"
@@ -49,17 +70,31 @@ if [ "$remote_mysql_host" = "localhost" ]; then
     remote_mysql_host="127.0.0.1"
 fi
 
-tunnelport=$(shuf -i 10000-20000 -n 1)
-echo "-- Choose random port for ssh-tunneling: $tunnelport"
+externalmysqlhost=$(jq -r '.externalmysqlhost' <<< "$shop_info")
 
-ssh -4 -f -L $tunnelport:$remote_mysql_host:$remote_mysql_port $host sleep 10; \
-  $mysqldump --opt --no-tablespaces --hex-blob -h 127.0.0.1 -P $tunnelport \
-  -u $remote_mysql_user -p"$remote_mysql_password" $remote_mysql_database \
-  --column-statistics=0 \
-  --complete-insert \
-  --add-drop-table \
-  --skip-lock-tables \
-  > tmp4dumps/$remote_mysql_database.sql
+if [ -z "$externalmysqlhost" ] || [ "$externalmysqlhost"  == null ]
+then
+
+  tunnelport=$(shuf -i 10000-20000 -n 1)
+  echo "-- Choose random port for ssh-tunneling: $tunnelport"
+
+  ssh -4 -f -L $tunnelport:$remote_mysql_host:$remote_mysql_port $host sleep 3; \
+    $mysqldump --opt --no-tablespaces --hex-blob -h 127.0.0.1 -P $tunnelport \
+    -u $remote_mysql_user -p"$remote_mysql_password" $remote_mysql_database \
+    --column-statistics=0 \
+    --complete-insert \
+    --add-drop-table \
+    --skip-lock-tables \
+    > tmp4dumps/$remote_mysql_database.sql
+else
+ $mysqldump --opt --no-tablespaces --hex-blob -h "$externalmysqlhost" -P $remote_mysql_port \
+    -u $remote_mysql_user -p"$remote_mysql_password" $remote_mysql_database \
+    --column-statistics=0 \
+    --complete-insert \
+    --add-drop-table \
+    --skip-lock-tables \
+    > tmp4dumps/$remote_mysql_database.sql
+fi
 
 # OPTIONAL: if dumping into an existing environment you could work with IF NOT EXISTS and REPLACE INTO-Alternatives
 # sed -i 's/CREATE TABLE /CREATE TABLE IF NOT EXISTS /g' tmp4dumps/$remote_mysql_database.sql
@@ -67,9 +102,13 @@ ssh -4 -f -L $tunnelport:$remote_mysql_host:$remote_mysql_port $host sleep 10; \
 
 sed -i 's/\sDEFINER=`[^`]*`@`[^`]*`//g' tmp4dumps/$remote_mysql_database.sql
 
-echo '... archiving the dumpfile.'
-DATE=$(date +"%Y%m%d-%H%M")
-zstd "tmp4dumps/$remote_mysql_database.sql" -o "tmp4dumps/$DATE-$remote_mysql_database.sql.zst"
 
-# OPTIONAL: clean up the unarchived dump
-# rm zstd "tmp4dumps/$remote_mysql_database.sql"
+if tail -n 1 "tmp4dumps/$remote_mysql_database.sql" | grep -q "^-- Dump completed on"; then
+    echo "Dump was completed successfully."
+    echo '... archiving the dumpfile.'
+    DATE=$(date +"%Y%m%d-%H%M")
+    zstd "tmp4dumps/$remote_mysql_database.sql" -o "tmp4dumps/$DATE-$remote_mysql_database.sql.zst"
+    rm "tmp4dumps/$remote_mysql_database.sql"
+else
+    echo "Error: The Dump file does NOT end with -- Dump completed. This could be an incomplete dump!"
+fi
